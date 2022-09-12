eabd571a23edbd120f1ae22f743b445c6ed167f2
Werner Stucky <werner@stucky-solutions.net>
2022-09-12 15:26:59 +0200

    added tailwindcss and skeleton components

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+
+  import '@brainandbones/skeleton/styles/themes/theme-rocket.css';
+  import "../app.css";
+
+  import { AppShell } from '@brainandbones/skeleton';
+  import { AppBar } from '@brainandbones/skeleton';
+
+</script>
+
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead">sse.dev</svelte:fragment>
+      Server-Sent-Events Playground Tool
+      <svelte:fragment slot="trail">...</svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- Be sure to insert your route <slot> in the default position --->
+
+	<slot />
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+  import { Card } from '@brainandbones/skeleton';
+  import { CodeBlock } from '@brainandbones/skeleton';
+
+  import hljs from 'highlight.js';
+  import 'highlight.js/styles/github-dark.css';
+  import { storeHighlightJs } from '@brainandbones/skeleton';
+storeHighlightJs.set(hljs);
+
+</script>
+
+
+
+<Card background="bg-primary-500" color="text-white">
+  <h1 class="text-3xl font-bold text-center">
+    Server-Sent-Events Playground Tool
+  </h1>
+
+  <p>This site allows you to play with Server-Sent Events easily.</p>
+
+
+  <h3 class="text-2xl font-bold text-center">Basic Usage</h3>
+  <p>
+    To receive the message in your browser copy and paste this into a script tag.  You will now be able to manipulate the DOM with the incoming data.<br>
+    You will receive a message by default every 2 seconds from the server.
+    <a href="/testpage">Demo Page to help you understand</a><br>
+    <a href="/test">Test Event Source here</a>
+  </p>
+
+  <CodeBlock language="javascript" code={`const evtSource = new EventSource("https://sse.dev/test");
+  evtSource.onmessage = function(event) {
+    var dataobj = JSON.parse(event.data);
+    console.log(dataobj);
+  }`}></CodeBlock>
+
+<!--
+  <h3>Basic Usage</h3>
+  <p>
+    To receive the message in your browser copy and paste this into a script tag.  You will now be able to manipulate the DOM with the incoming data.<br>
+    You will receive a message by default every 2 seconds from the server.
+    <pre>
+    const evtSource = new EventSource("https://sse.dev/test");
+    evtSource.onmessage = function(event) {
+      var dataobj = JSON.parse(event.data);
+      console.log(dataobj);
+    }
+    </pre>
+    <a href="/testpage">Demo Page to help you understand</a><br>
+    <a href="/test">Test Event Source here</a>
+  </p>
+
+
+
+  <h2>Advanced</h2>
+  <h3>Setting the interval</h3>
+  <p>
+    To set the interval pass a "interval" query parameter on the url. eg. for 5 seconds do the following
+    <pre>
+    const evtSource = new EventSource("https://sse.dev/test?interval=5");
+    </pre>
+    <a href="/test?interval=5">Test Event Source here</a>
+  </p>
+
+
+
+  <h3>Setting the data returned every interval</h3>
+  <p>
+    To set the JSON data returned for each interval, pass a "jsonobj" query parameter on the url. eg.
+    <pre>
+    const evtSource = new EventSource("https://sse.dev/test?jsonobj={"name":"werner","age":38}");
+    </pre>
+    <a href='/test?jsonobj={"name":"werner","age":38}'>Test Event Source here</a>
+  </p> -->
+
+
+</Card>
+
+
